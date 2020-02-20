0273aafccc44b6de618d096dde8f1096bfc21ba4
Gayani Madahapola <gayanimadahapola@users.github.com>
2020-02-20 10:05:53 +0530

    Create UI for LOGIN and SignUp

---
+++ GayaniMadahapola-cobsccomp182p-002/Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  GayaniMadahapola-cobsccomp182p-002
+//
+//  Created by Gayani Madubhashini on 2/20/20.
+//  Copyright © 2020 Gayani Madubhashini. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
