cfa8e20051a8d7292a0cc487370df4ceeccc056f
Gayani Madahapola <gayanimadahapola@users.github.com>
2020-02-28 02:38:07 +0530

    Added reference to Login & Signup

--- GayaniMadahapola-cobsccomp182p-002/Controllers/ViewController.swift
+++ GayaniMadahapola-cobsccomp182p-002/Controllers/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
